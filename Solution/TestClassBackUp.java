169485e5306581b5269bea0d3c187c19e0435725
Yahav Rashuk <rashuk.yahav@gmail.com>
2020-05-17 13:04:32 +0300

    yahav

--- Solution/TestClassBackUp.java
+++ Solution/TestClassBackUp.java
@@ -42,6 +42,59 @@ public class TestClassBackUp {
     public void setObject_backup(Object object_backup) {
         this.object_backup = object_backup;
     }
+//    public void backUpObject(Object object) throws IllegalAccessException {
+//        flag_backup=true;
+//        //Field[] fields = object.getClass().getFields();
+//        Field[] fields = object.getClass().getDeclaredFields();
+//        for (Field f : fields) {
+//            f.setAccessible(true);
+//            if (f == null) {
+//                f.set(object_backup, null);
+//            } else {
+//                for (Class<?> inter : f.getClass().getInterfaces()) {
+//                    if (inter.equals(Cloneable.class)) {
+//                        try {
+//                            Method clone_method = f.getClass().getDeclaredMethod("clone");
+//                            clone_method.setAccessible(true);
+//                            f.set(object_backup, clone_method.invoke(f));
+//                        } catch (NoSuchMethodException e) {
+//                            try {
+//                                Constructor<?> cons = f.getClass().getDeclaredConstructor(f.getClass());
+//                                cons.setAccessible(true);
+//                                try {
+//                                    f.set(object_backup, cons.newInstance(f));
+//                                } catch (InstantiationException instantiationException) {
+//                                    f.set(object_backup, f.get(object));
+//                                } catch (InvocationTargetException invocationTargetException) {
+//                                    invocationTargetException.printStackTrace();
+//                                }
+//                            } catch (NoSuchMethodException noSuchMethodException) {
+//                                noSuchMethodException.printStackTrace();
+//                            }
+//                        } catch (InvocationTargetException e) {
+//                            e.printStackTrace();
+//                        }
+//                    }
+//                }
+//                try {
+//                    Constructor<?> cons = null;
+//                    cons = f.getClass().getDeclaredConstructor(f.getClass());
+//                    cons.setAccessible(true);
+//                    f.set(object_backup, cons.newInstance(f));
+//                } catch (NoSuchMethodException e) {
+//                    f.set(object_backup, f.get(object));
+//                } catch (InstantiationException e) {
+//                    e.printStackTrace();
+//                } catch (InvocationTargetException e) {
+//                    e.printStackTrace();
+//                }
+//            }
+//        }
+//    }
+//
+
+
+
     public void backUpObject(Object object) throws IllegalAccessException {
         flag_backup=true;
         //Field[] fields = object.getClass().getFields();
@@ -52,43 +105,34 @@ public class TestClassBackUp {
                 f.set(object_backup, null);
             } else {
                 for (Class<?> inter : f.getClass().getInterfaces()) {
-                    if (inter.equals(Cloneable.class)) {
+                    if (inter.equals(Cloneable.class)) {        //if object cloneable
                         try {
                             Method clone_method = f.getClass().getDeclaredMethod("clone");
                             clone_method.setAccessible(true);
                             f.set(object_backup, clone_method.invoke(f));
-                        } catch (NoSuchMethodException e) {
+                        } catch (Exception e) {   // if we have exception call copy constructor
                             try {
                                 Constructor<?> cons = f.getClass().getDeclaredConstructor(f.getClass());
                                 cons.setAccessible(true);
-                                try {
-                                    f.set(object_backup, cons.newInstance(f));
-                                } catch (InstantiationException instantiationException) {
-                                    f.set(object_backup, f.get(object));
-                                } catch (InvocationTargetException invocationTargetException) {
-                                    invocationTargetException.printStackTrace();
-                                }
-                            } catch (NoSuchMethodException noSuchMethodException) {
-                                noSuchMethodException.printStackTrace();
+                                f.set(object_backup, cons.newInstance(f));
+                            } catch (Exception a) {               //if we have exception shllw copt
+                                f.set(object_backup, f.get(object));
                             }
-                        } catch (InvocationTargetException e) {
-                            e.printStackTrace();
                         }
                     }
                 }
-                try {
+                try {      //try copy constructor
                     Constructor<?> cons = null;
                     cons = f.getClass().getDeclaredConstructor(f.getClass());
                     cons.setAccessible(true);
                     f.set(object_backup, cons.newInstance(f));
-                } catch (NoSuchMethodException e) {
+                } catch (Exception e) {             //if we have exception shllw copy
                     f.set(object_backup, f.get(object));
-                } catch (InstantiationException e) {
-                    e.printStackTrace();
-                } catch (InvocationTargetException e) {
-                    e.printStackTrace();
                 }
             }
         }
     }
+
+
+
 }
